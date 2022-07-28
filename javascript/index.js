1b4d157fc7bb92626fca9d3d22577b18867b997e
Shibily-kms <kmsshibily@gmail.com>
2022-07-28 14:45:11 +0530

    change sume ui

--- javascript/index.js
+++ javascript/index.js
@@ -21,27 +21,5 @@ function hideMenu() {
         icon.style.display = 'block';
         hide.style.display = 'none';
 
-   
-
 }
 
-
-// function reveal() {
-//     var reveals = document.querySelectorAll(".reveal");
-  
-//     for (var i = 0; i < reveals.length; i++) {
-//       var windowHeight = window.innerHeight;
-//       var elementTop = reveals[i].getBoundingClientRect().top;
-//       var elementVisible = 150;
-  
-//       if (elementTop < windowHeight - elementVisible) {
-//         reveals[i].classList.add("active");
-//       } else {
-//         reveals[i].classList.remove("active");
-//       }
-//     }
-//   }
-  
-//   window.addEventListener("scroll", reveal);
-
-// Change section four backgorund image
